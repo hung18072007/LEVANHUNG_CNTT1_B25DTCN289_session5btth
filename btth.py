c6735dfe6840ebb0df064c874ba46535959e3710
hung18072007 <lehung187207@gmail.com>
2026-05-21 20:51:09 +0700

    Add files via upload

---
+++ btth.py
@@ -0,0 +1,28 @@
+# 4.1. Nhập số lượng nhân viên cần kiểm tra
+employee_count = int(input("Nhập số lượng nhân viên: "))
+
+for i in range(1, employee_count + 1):
+
+    employee_name = input("Nhập tên nhân viên: ")
+    working_days = int(input("Nhập số ngày làm: "))
+    
+    if working_days < 0 or working_days > 22:
+        print("Dữ liệu không hợp lệ")
+        continue  
+
+    if working_days == 0:
+        print("Nhân viên nghỉ toàn bộ tháng")
+        continue 
+
+    print(employee_name, end=": ")
+    for d in range(working_days):
+        print("*", end="") 
+    print() 
+    
+    # 4.6. Thống kê mức độ làm việc
+    if working_days >= 18:
+        print("Làm việc chăm chỉ")
+    elif working_days < 10:
+        print("Làm việc ít")
+    else:
+        print("Làm việc bình thường")
